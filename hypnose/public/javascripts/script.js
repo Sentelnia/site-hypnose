91e532f01e09fd3d09b50e426d33cf940f279b53
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-05-28 18:19:37 +0200

    edition dashboard

--- hypnose/public/javascripts/script.js
+++ hypnose/public/javascripts/script.js
@@ -3,3 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+
+
+//creation d'une div ou taper son mdp pour valider les chagements du profil.
+const $editProfilDiv = document.querySelector('#edit_profil')
+const $button = document.querySelector('#buttonEdit')
+const $edit = document.querySelector("#edit")
+const $confirmMdp = document.createElement('label');
+const $champMdp = document.createElement('input');
+const $buttonValid = document.createElement('button');
+
+
+
+$champMdp.name = 'password'
+$champMdp.type = 'password'
+$confirmMdp.innerHTML = 'Veuillez tapez votre Mot de passe pour éditer vos infos perso'
+$buttonValid.innerHTML = 'Editer mes infos perso'
+
+
+// ajout du label mdp dans le form
+$button.addEventListener("click", () => {
+  $edit.appendChild($confirmMdp)
+  $confirmMdp.appendChild($champMdp)
+  $edit.appendChild($buttonValid)
+  $editProfilDiv.removeChild($button)
+})
--- hypnose/routes/auth.js
+++ hypnose/routes/auth.js
@@ -1,74 +1,105 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = new Router();
-const User = require('../models/User.js')
+const User = require("../models/User.js");
 
-const bcrypt = require('bcryptjs');
-const salt = bcrypt.genSaltSync(10)
+const bcrypt = require("bcryptjs");
+const salt = bcrypt.genSaltSync(10);
 
 // Add passport
-const passport = require('passport');
-const ensureLogin = require('connect-ensure-login');
-
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
 
 ////////////SIGNUP//////////////////////
 
-router.get('/inscription', (req, res) => res.render('auth/inscription'));
+router.get("/inscription", (req, res) => res.render("auth/inscription"));
 
-
-//-----------1. créer la session -------------//
-//-----------2. validation mdp (créer un nouveau champs) ----------------//
-router.post('/inscription', (req, res, next) => {
+//-----------1. validation mdp (créer un nouveau champs) ----------------//
+router.post("/inscription", (req, res, next) => {
   const { name, last_name, email, tel, password } = req.body;
 
-
   //si pas de mail, pas de password , pas de name et pas last_name, on rend le formulaire d'inscription
-  if (!name || !last_name || !email || !tel || !password ) {
-    res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
+  if (!name || !last_name || !email || !tel || !password) {
+    res.render("auth/signup", {
+      errorMessage:
+        "All fields are mandatory. Please provide your username, email and password.",
+    });
     return;
   }
 
-  const hashedPassword = bcrypt.hashSync(password, salt)
-  console.log(`Password hash: ${hashedPassword}`);  
+  const hashedPassword = bcrypt.hashSync(password, salt);
+  console.log(`Password hash: ${hashedPassword}`);
 
   User.create({
     name: name,
     last_name: last_name,
     email: email,
     tel: tel,
-    password: hashedPassword
+    password: hashedPassword,
+  })
+    .then((userFromDB) => {
+      console.log("Newly created user is: ", userFromDB);
+      res.redirect("/");
     })
-      .then(userFromDB => {
-        console.log('Newly created user is: ', userFromDB);
-        res.redirect('/')
-      })
-      .catch(error => next(error));
+    .catch((error) => next(error));
 });
-
 
 ////////////LOGIN//////////////////////
 
-router.get('/login', (req, res, next) => {
-  res.render('auth/login', { errorMessage: req.flash('error') }); // !!!
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { errorMessage: req.flash("error") }); // !!!
 });
 
-
-//-----------2. attention mail en minuscule à configurer -------------//
-//-----------2. créer la session et les cookies -------------//
+//-----------1. attention mail en minuscule à configurer -------------//
 router.post(
-  '/login',
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true 
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+    failureFlash: true,
   })
 );
 
-
 ////////////LOGOUT//////////////////////
 
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.logout();
-  res.redirect('/login');
+  res.redirect("/login");
+});
+
+////////////DASHBOARD//////////////////////
+
+router.get("/dashboard", (req, res) => {
+  console.log(req.user);
+  if (!req.user) {
+    res.render("auth/login", {
+      errorMessage: "vous devez vous identifier pour acceder à ce contenu",
+    });
+    return;
+  }
+  res.render("auth/dashboard", { user: req.user });
+});
+
+//---------edit infos perso-------------//
+router.post("/dashboard", (req, res, next) => {
+  const password = req.body.password;
+
+  if (bcrypt.compareSync(password, req.user.password)) {
+    User.update(
+      { _id: req.user.id },
+      {
+        $set: {
+          name: req.body.name,
+          last_name: req.body.last_name,
+          email: req.body.email,
+          tel: req.body.tel,
+        },
+      }
+    )
+      .then(() => res.redirect("/dashboard"))
+      .catch((err) => next(err));
+  } else {
+    res.render("auth/dashboard", { errorMessage: 'Mot de passe non valid' })
+  }
 });
 
 module.exports = router;
