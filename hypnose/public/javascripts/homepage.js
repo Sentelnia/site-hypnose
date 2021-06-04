fff254e8f1d99e519deda424e235e3ad66a1fbe2
Projetphp <alexandre.capaldi@hotmail.fr>
2021-06-04 17:05:41 +0200

    fixing menu

---
+++ hypnose/public/javascripts/homepage.js
@@ -0,0 +1,19 @@
+let $mobile = document.querySelector("#mobile")
+let $burger = 
+
+function burgerMenu() {
+
+    $mobile.classList.toggle("active");
+
+}
+
+function closeMenu() {
+
+    $mobile.classList.toggle("active");
+
+}
+
+
+document.getElementById("myBtn").addEventListener("click", function() {
+    alert("Hello World!");
+  });
