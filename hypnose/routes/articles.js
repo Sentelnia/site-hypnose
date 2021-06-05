0ed51c0ce71f585718dff20f6ccffc0197dbb29b
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-06-05 12:40:02 +0200

    amelio like + user pour les template

--- hypnose/routes/articles.js
+++ hypnose/routes/articles.js
@@ -1,287 +1,290 @@
-const {
-  Router
-} = require("express");
+const { Router } = require("express");
 const router = new Router();
 
 const Article = require("../models/Article.js");
 const User = require("../models/User.js");
-const fileUploader = require('../configs/cloudinary.config');
+const fileUploader = require("../configs/cloudinary.config");
 
 // require slugify
 
-const slugify = require('slugify')
-
+const slugify = require("slugify");
 
 ////////////LISTE DES ARTICLES////////////////////
 
-router.get('/articles', (req, res, next) => {
+router.get("/articles", (req, res, next) => {
   Article.find()
-    .then(allArticleFromDB => {
+    .then((allArticleFromDB) => {
       allArticleFromDB.forEach((article) => {
-        article.title2 = slugify(article.title)
-      })
-      if (req.isAuthenticated() && req.user.role === 'ADMIN') {
-
-        res.render('articles/All-articles', {
+        article.title2 = slugify(article.title);
+      });
+      if (!req.isAuthenticated()) {
+        res.render("articles/All-articles", {
           articles: allArticleFromDB,
-          message: 'admin'
-        })
-        return
-      } else {
-        res.render('articles/All-articles', {
-          articles: allArticleFromDB
-        })
+        });
+        return;
+      } else if (req.isAuthenticated() && req.user.role === "ADMIN") {
+        res.render("articles/All-articles", {
+          articles: allArticleFromDB,
+          message: "admin",
+        });
+        return;
       }
     })
-    .catch(err => next(err))
-})
-
-
-
+    .catch((err) => next(err));
+});
 
 ////////////CREER UN ARTICLE////////////////////
 
-router.get('/articles/create', checkRoles('ADMIN'), (req, res) => {
-  res.render('articles/create')
-})
+router.get("/articles/create", checkRoles("ADMIN"), (req, res) => {
+  res.render("articles/create");
+});
 
-router.post('/articles/create', fileUploader.single('image'), (req, res, next) => {
-  const {
-    title,
-    description,
-    video
-  } = req.body;
+router.post(
+  "/articles/create",
+  fileUploader.single("image"),
+  (req, res, next) => {
+    const { title, description, video } = req.body;
 
-
-  // Condition sans image
-  if (req.file === undefined) {
-    Article.create({
-        title,
-        description,
-        video,
-        like: 0
-      })
-      .then(() => res.redirect('/articles'))
-      .catch(err => next(err))
-    return
-    // Condition avec image
-  } else {
-    Article.create({
-        title,
-        description,
-        video,
-        imageUrl: req.file.path,
-        like: 0
-      })
-      .then(() => res.redirect('/articles'))
-      .catch(err => next(err))
+    if (!req.isAuthenticated()) {
+      res.redirect("/login");
+      return;
+    } else {
+      // Condition sans image
+      if (req.file === undefined) {
+        Article.create({
+          title,
+          description,
+          video,
+          like: 0,
+        })
+          .then(() => res.redirect("/articles"))
+          .catch((err) => next(err));
+        return;
+        // Condition avec image
+      } else {
+        Article.create({
+          title,
+          description,
+          video,
+          imageUrl: req.file.path,
+          like: 0,
+        })
+          .then(() => res.redirect("/articles"))
+          .catch((err) => next(err));
+      }
+    }
   }
-})
-
-
+);
 
 ////////////EDITER UN ARTICLE////////////////////
 
-router.get('/articles/:articleId/edit', checkRoles('ADMIN'), (req, res, next) => {
-  const {
-    articleId
-  } = req.params;
+router.get(
+  "/articles/:articleId/edit",
+  checkRoles("ADMIN"),
+  (req, res, next) => {
+    const { articleId } = req.params;
 
-
-  Article.findById(articleId)
-    .then(articleToEdit => {
-      res.render('articles/edit', {
-        article: articleToEdit
+    Article.findById(articleId)
+      .then((articleToEdit) => {
+        res.render("articles/edit", {
+          article: articleToEdit,
+        });
       })
-    })
-    .catch(err => next(err))
-})
-
-router.post('/articles/:articleId/edit', fileUploader.single('image'), (req, res, next) => {
-  const {
-    articleId
-  } = req.params;
-  const {
-    title,
-    description,
-    video,
-    like
-  } = req.body;
-  console.log(req.file)
-
-
-  // Condition sans image
-  if (req.file === undefined) {
-    Article.findByIdAndUpdate(articleId, {
-        title,
-        description,
-        video,
-        like,
-        imageUrl: ''
-      })
-      .then((updatedArticle) => {
-        updatedArticle.title2 = slugify(updatedArticle.title)
-        res.redirect(`/articles/${updatedArticle.title2}/${updatedArticle.id}`)
-      })
-      .catch(err => next(err))
-    return
-    // Condition avec image
-  } else {
-    Article.findByIdAndUpdate(articleId, {
-        title,
-        description,
-        video,
-        imageUrl: req.file.path,
-        like
-      })
-      .then((updatedArticle) => {
-        updatedArticle.title2 = slugify(updatedArticle.title)
-        res.redirect(`/articles/${updatedArticle.title2}/${updatedArticle.id}`)
-      })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   }
-})
+);
 
+router.post(
+  "/articles/:articleId/edit",
+  fileUploader.single("image"),
+  (req, res, next) => {
+    const { articleId } = req.params;
+    const { title, description, video, like } = req.body;
 
-
+    if (!req.isAuthenticated()) {
+      res.redirect("/login");
+      return;
+    } else {
+      // Condition sans image
+      if (req.file === undefined) {
+        Article.findByIdAndUpdate(articleId, {
+          title,
+          description,
+          video,
+          like,
+          imageUrl: "",
+        })
+          .then((updatedArticle) => {
+            updatedArticle.title2 = slugify(updatedArticle.title);
+            res.redirect(
+              `/articles/${updatedArticle.title2}/${updatedArticle.id}`
+            );
+          })
+          .catch((err) => next(err));
+        return;
+        // Condition avec image
+      } else {
+        Article.findByIdAndUpdate(articleId, {
+          title,
+          description,
+          video,
+          imageUrl: req.file.path,
+          like,
+        })
+          .then((updatedArticle) => {
+            updatedArticle.title2 = slugify(updatedArticle.title);
+            res.redirect(
+              `/articles/${updatedArticle.title2}/${updatedArticle.id}`
+            );
+          })
+          .catch((err) => next(err));
+      }
+    }
+  }
+);
 
 ////////////SUPPRIMER UN ARTICLE EN LISTE////////////////////
 
-router.post('/articles/:articleId/delete', checkRoles('ADMIN'), (req, res, next) => {
-  const {
-    articleId
-  } = req.params;
+router.post(
+  "/articles/:articleId/delete",
+  checkRoles("ADMIN"),
+  (req, res, next) => {
+    const { articleId } = req.params;
 
-  Article.findByIdAndDelete(articleId)
-    .then(() => res.redirect('/articles'))
-    .catch(err => next(err))
-})
-
+    if (!req.isAuthenticated()) {
+      res.redirect("/login");
+      return;
+    } else {
+      Article.findByIdAndDelete(articleId)
+        .then(() => res.redirect("/articles"))
+        .catch((err) => next(err));
+    }
+  }
+);
 
 ////////////AJOUTE UN ARTICLE LIKE DANS LE PROFIL DU USER////////////////////
 
 router.post("/articles/:articleId/like", (req, res, next) => {
-<<<<<<< HEAD
   const { articleId } = req.params;
-  
-  console.log(req.body.like)
-  if(req.isAuthenticated()){
-    const { articles_like } = req.user
-    
-=======
-  const {
-    articleId
-  } = req.params;
 
-  if (req.isAuthenticated()) {
-    const {
-      articles_like
-    } = req.user
+  if (!req.isAuthenticated()) {
+    res.redirect("/login");
+    return;
+  } else {
+    const { articles_like } = req.user;
 
->>>>>>> 0e724d1153fb5d11dec1c8a3d1662751c27caaf2
     Article.findById(articleId)
       .then((article) => {
-        article.title2 = slugify(article.title)
-        articles_like.push(article)
-        User.findByIdAndUpdate(req.user.id, {
-            articles_like
+        User.findById(req.user.id)
+          .then((user) => {
+            if (user.articles_like.includes(article._id)) {
+              res.redirect(`/articles`);
+              return;
+            } else {
+              article.title2 = slugify(article.title);
+              articles_like.push(article);
+              User.findByIdAndUpdate(req.user.id, {
+                articles_like,
+              })
+                .then(() => {
+                  res.redirect(`/articles`);
+                })
+                .catch((err) => next(err));
+            }
           })
-          .then(() => {
-            res.redirect(`/articles`)
-          })
-          .catch(err => next(err))
+          .catch((err) => next(err));
       })
-      .catch(err => next(err))
-    return
+      .catch((err) => next(err));
+    return;
   }
-})
-
+});
 
 ////////////SUPPRIME UN ARTICLE LIKE DANS LE PROFIL DU USER////////////////////
 
-router.post('/articles/:articleId/dislike', (req, res, next) => {
-  const {
-    articleId
-  } = req.params;
+router.post("/articles/:articleId/dislike", (req, res, next) => {
+  const { articleId } = req.params;
 
-  if (req.isAuthenticated()) {
-    const {
-      articles_like
-    } = req.user
+  if (!req.isAuthenticated()) {
+    res.redirect("/login");
+    return;
+  } else {
+    const { articles_like } = req.user;
 
     Article.findById(articleId)
       .then((article) => {
-        article.title2 = slugify(article.title)
-        articles_like.splice(articles_like.indexOf(article), 1)
+        console.log(`article cliqué : ${article}`);
+        article.title2 = slugify(article.title);
+        articles_like.splice((articles_like.indexOf(article._id)), 1);
         User.findByIdAndUpdate(req.user.id, {
-            articles_like
-          })
+          articles_like,
+        })
           .then(() => {
-            res.redirect(`/dashboard`)
+            res.redirect(`/dashboard`);
           })
-          .catch(err => next(err))
+          .catch((err) => next(err));
       })
-      .catch(err => next(err))
-    return
+      .catch((err) => next(err));
+    return;
   }
-})
-
-
+});
 
 ////////////DETAIL POUR UN ARTICLE////////////////////
 
-router.get('/articles/:articleName/:articleId', (req, res, next) => {
-  const {
-    articleId
-  } = req.params;
-
+router.get("/articles/:articleName/:articleId", (req, res, next) => {
+  const { articleId } = req.params;
 
   Article.findById(articleId)
-    .then(article => {
-      res.render('articles/article-details', {
-        article: article
-      })
+    .then((article) => {
+      res.render("articles/article-details", {
+        article: article,
+      });
     })
 
-    .catch(err => next(err))
-})
-
+    .catch((err) => next(err));
+});
 
 ////////////EDITER UN ARTICLE EN DETAIL////////////////////
 
-router.get('/articles/:articleName/:articleId/edit', checkRoles('ADMIN'), (req, res) => {
-  const {
-    articleId
-  } = req.params;
+router.get(
+  "/articles/:articleName/:articleId/edit",
+  checkRoles("ADMIN"),
+  (req, res) => {
+    const { articleId } = req.params;
 
-  Article.findById(articleId)
-    .then(articleToEdit => {
-      articleToEdit.title2 = slugify(articleToEdit.title)
-      res.render('articles/edit', {
-        article: articleToEdit
-      })
-    })
-    .catch(err => next(err))
-})
-
-
+    if (!req.isAuthenticated()) {
+      res.redirect("/login");
+      return;
+    } else {
+      Article.findById(articleId)
+        .then((articleToEdit) => {
+          articleToEdit.title2 = slugify(articleToEdit.title);
+          res.render("articles/edit", {
+            article: articleToEdit,
+          });
+        })
+        .catch((err) => next(err));
+    }
+  }
+);
 
 ////////////SUPPRIMER UN ARTICLE EN DETAIL////////////////////
 
-router.post('/articles/:articleName/:articleId/delete', checkRoles('ADMIN'), (req, res, next) => {
-  const {
-    articleId
-  } = req.params;
+router.post(
+  "/articles/:articleName/:articleId/delete",
+  checkRoles("ADMIN"),
+  (req, res, next) => {
+    const { articleId } = req.params;
 
-  Article.findByIdAndDelete(articleId)
-    .then(() => res.redirect('/articles'))
-    .catch(err => next(err))
-})
-
-
-
-
-
+    if (!req.isAuthenticated()) {
+      res.redirect("/login");
+      return;
+    } else {
+      Article.findByIdAndDelete(articleId)
+        .then(() => res.redirect("/articles"))
+        .catch((err) => next(err));
+    }
+  }
+);
 
 ///////////////////////FONCTION POUR ENLEVER LES ESPACES DANS L'URL////////////////////
 
@@ -289,19 +292,18 @@ router.post('/articles/:articleName/:articleId/delete', checkRoles('ADMIN'), (re
 //   // ex str : "test 2"
 //   let result = str.replace(' ', '-')
 //   return result
-// } 
-
+// }
 
 ///////////////////////FONCTION POUR LES ROLES////////////////////
 
 function checkRoles(role) {
   return function (req, res, next) {
-    if (req.isAuthenticated() && req.user.role === 'ADMIN') {
+    if (req.isAuthenticated() && req.user.role === "ADMIN") {
       return next();
     } else {
-      res.redirect('/login');
+      res.redirect("/login");
     }
   };
 }
 
-module.exports = router
+module.exports = router;
--- hypnose/routes/temoignages.js
+++ hypnose/routes/temoignages.js
@@ -1,90 +1,113 @@
 const { Router } = require("express");
 const router = new Router();
 
-
 const Temoignage = require("../models/Temoignage.js");
-
 
 ////////////LISTE DES TEMOIGNAGES//////////////////
 
-router.get('/temoignages', (req , res, next) => {
+router.get("/temoignages", (req, res, next) => {
   Temoignage.find()
-  .then(allTemoignagesFromDB => {
-    if (req.isAuthenticated() && req.user.role === 'ADMIN') {
-      res.render('temoignages/All-temoignages', {temoignages : allTemoignagesFromDB , message:'admin'})
-    return
-    } else {
-      res.render('temoignages/All-temoignages', {temoignages : allTemoignagesFromDB})
-    }
-  })
-  .catch(err => next(err))
-})
-
-
+    .then((allTemoignagesFromDB) => {
+      if (req.isAuthenticated() && req.user.role === "ADMIN") {
+        res.render("temoignages/All-temoignages", {
+          temoignages: allTemoignagesFromDB,
+          message: "admin",
+        });
+        return;
+      } else {
+        res.render("temoignages/All-temoignages", {
+          temoignages: allTemoignagesFromDB,
+        });
+      }
+    })
+    .catch((err) => next(err));
+});
 
 ////////////CREER UN TEMOIGNAGE////////////////////
 
-router.get('/temoignages/create', checkRoles('ADMIN'), (req, res) => {
-  res.render('temoignages/create')
-})
+router.get("/temoignages/create", checkRoles("ADMIN"), (req, res) => {
+  res.render("temoignages/create");
+});
 
-router.post('/temoignages/create', (req, res, next) => {
+router.post("/temoignages/create", (req, res, next) => {
   const { type_seance, message, signature } = req.body;
 
-  Temoignage.create({type_seance, message, signature})
-  .then(() => res.redirect('/temoignages'))
-  .catch(err => next(err))
-})
-
-
+  if (!req.isAuthenticated()) {
+    res.redirect("/login");
+    return;
+  } else {
+    Temoignage.create({ type_seance, message, signature })
+      .then(() => res.redirect("/temoignages"))
+      .catch((err) => next(err));
+  }
+});
 
 ////////////EDITER UN TEMOIGNAGE////////////////////
 
-router.get('/temoignages/:temoignageId/edit', checkRoles('ADMIN'), (req, res) =>{
-  const { temoignageId } = req.params;
+router.get(
+  "/temoignages/:temoignageId/edit",
+  checkRoles("ADMIN"),
+  (req, res) => {
+    const { temoignageId } = req.params;
 
-  Temoignage.findById(temoignageId)
-  .then(temoignageToEdit => {
-    res.render('temoignages/edit',{temoignage : temoignageToEdit})
-  })
-  .catch(err => next(err))
-})
+    Temoignage.findById(temoignageId)
+      .then((temoignageToEdit) => {
+        res.render("temoignages/edit", { temoignage: temoignageToEdit });
+      })
+      .catch((err) => next(err));
+  }
+);
 
-router.post('/temoignages/:temoignageId/edit', (req, res,next) =>{
+router.post("/temoignages/:temoignageId/edit", (req, res, next) => {
   const { temoignageId } = req.params;
   const { type_seance, message, signature } = req.body;
-  console.log(req.body)
 
-  Temoignage.findByIdAndUpdate(temoignageId, {type_seance, message, signature})
-    .then(()=> {
-      console.log('yop')
-      res.redirect(`/temoignages`)})
-    .catch(err => next(err))
-})
-
-
+  if (!req.isAuthenticated()) {
+    res.redirect("/login");
+    return;
+  } else {
+    Temoignage.findByIdAndUpdate(temoignageId, {
+      type_seance,
+      message,
+      signature,
+    })
+      .then(() => {
+        console.log("yop");
+        res.redirect(`/temoignages`);
+      })
+      .catch((err) => next(err));
+  }
+});
 
 ////////////SUPPRIMER UN TEMOIGNAGE////////////////////
 
-router.post('/temoignages/:temoignageId/delete', checkRoles('ADMIN'), (req, res, next) => {
-  const { temoignageId } = req.params;
+router.post(
+  "/temoignages/:temoignageId/delete",
+  checkRoles("ADMIN"),
+  (req, res, next) => {
+    const { temoignageId } = req.params;
 
-  Temoignage.findByIdAndDelete(temoignageId)
-    .then(() => res.redirect('/temoignages'))
-    .catch(err => next(err))
-})
-
+    if (!req.isAuthenticated()) {
+      res.redirect("/login");
+      return;
+    } else {
+      Temoignage.findByIdAndDelete(temoignageId)
+        .then(() => res.redirect("/temoignages"))
+        .catch((err) => next(err));
+    }
+  }
+);
 
 ///////////////////////FONCTION POUR LES ROLES////////////////////
 
 function checkRoles(role) {
   return function (req, res, next) {
-    if (req.isAuthenticated() && req.user.role === 'ADMIN') {
+    if (req.isAuthenticated() && req.user.role === "ADMIN") {
       return next();
     } else {
-      res.redirect('/login');
+      res.redirect("/login");
     }
   };
 }
 
-module.exports = router
+module.exports = router;
